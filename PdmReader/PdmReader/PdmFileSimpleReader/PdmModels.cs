909a1079c75e08fc20436e2e5753b0789f4a04a0
wwbgo <wwbgo@qq.com>
2014-04-21 17:21:07 +0800

    PDMReader Object Signed-off-by: wwbgo <wwbgo@qq.com>

---
+++ PdmReader/PdmReader/PdmFileSimpleReader/PdmModels.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PdmFileSimpleReader {
+    /// <summary>  
+    /// PDM实体集合  
+    /// </summary>  
+    public class PdmModels {
+        public PdmModels() {
+            Tables = new List<TableInfo>();
+            Views = new List<ViewInfo>();
+        }
+        /// <summary>  
+        /// 表集合  
+        /// </summary>  
+        public IList<TableInfo> Tables {
+            get;
+            private set;
+        }
+        /// <summary>  
+        /// 视图集合  
+        /// </summary>  
+        public IList<ViewInfo> Views {
+            get;
+            private set;
+        }
+    }
+}
