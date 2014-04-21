eccc3de51934b2d60e582098397a3c105bc2d696
wwbgo <wwbgo@qq.com>
2014-04-21 19:42:46 +0800

    Object Commit Signed-off-by: wwbgo <wwbgo@qq.com>

---
+++ PdmReader/PdmReader/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PdmFileSimpleReader {
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
