15a887aeee86a45ee2f3f16e94862949fd58ca67
wwbgo <wwbgo@qq.com>
2014-04-23 17:29:05 +0800

    Signed-off-by: wwbgo <wwbgo@qq.com>

--- PdmReader/PdmReader/MainWindow.xaml.cs
+++ PdmReader/PdmReader/MainWindow.xaml.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
 using PdmReader.Models;
+using PdmReader.Models.PdmModels;
 using FolderBrowserDialog = System.Windows.Forms.FolderBrowserDialog;
 
 namespace PdmReader {
-    public partial class MainWindow : INotifyPropertyChanged {
+    public partial class PdmReaderWindow : INotifyPropertyChanged {
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged(string propertyName) {
             var handler = PropertyChanged;
@@ -13,7 +16,7 @@ namespace PdmReader {
                 handler(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public MainWindow() {
+        public PdmReaderWindow() {
             InitializeComponent();
             Folder.Focus();
         }
@@ -29,18 +32,58 @@ namespace PdmReader {
             }
         }
 
+        private ObservableCollection<string> _listShows;
+        public ObservableCollection<string> ListShows {
+            get {
+                return _listShows;
+            }
+            set {
+                _listShows = value;
+                OnPropertyChanged("ListShows");
+            }
+        }
+
+        private IEnumerable<string> GetPdmFiles(string dir, string searchPattern) {
+            return string.IsNullOrEmpty(searchPattern) ? GetFiles.GetPdmFiles(dir) : GetFiles.GetPdmFiles(dir, searchPattern);
+        }
+
+        private string SelectedPath {
+            get;
+            set;
+        }
         private void Folder_Click(object sender, RoutedEventArgs e) {
-            var fbd = new FolderBrowserDialog();
+            var fbd = new FolderBrowserDialog {
+                ShowNewFolderButton = false,
+                SelectedPath = SelectedPath
+            };
             var result = fbd.ShowDialog();
             if(result == System.Windows.Forms.DialogResult.Cancel)
                 return;
             Folder.Text = fbd.SelectedPath;
-            ListShow.ItemsSource = GetFiles.GetPdmFiles(Folder.Text);
+            SelectedPath = fbd.SelectedPath;
+            ListShow.ItemsSource = GetPdmFiles(Folder.Text, string.Format("*{0}*.pdm", SqlCheck.SelectedItem != null && SqlCheck.SelectionBoxItem.ToString() != "All" ? SqlCheck.SelectionBoxItem.ToString() : string.Empty));
         }
 
         private void Folder_KeyDown(object sender, KeyEventArgs e) {
-            if(e.Key == Key.Return)
-                ListShow.ItemsSource = GetFiles.GetPdmFiles(Folder.Text);
+            if(e.Key == Key.Return) {
+                ListShow.ItemsSource = GetPdmFiles(Folder.Text, string.Format("*{0}*.pdm", SqlCheck.SelectedItem != null && SqlCheck.SelectionBoxItem.ToString() != "All" ? SqlCheck.SelectionBoxItem.ToString() : string.Empty));
+                SelectedPath = Folder.Text;
+            }
+        }
+
+        private void ListShow_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            if(ListShow.SelectedItem == null) return;
+            var tableWindow = new TableWindow {
+                Title = ListShow.SelectedValue.ToString()
+            };
+            var pdmFileReader = new PdmFileReader();
+            var pdmModels = pdmFileReader.ReadFromFile(ListShow.SelectedValue.ToString());
+            tableWindow.DataContext = pdmModels;
+            tableWindow.Show();
+        }
+
+        private void Folder_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            Folder.SelectAll();
         }
     }
 }
--- PdmReader/PdmReader/Models/GetFiles.cs
+++ PdmReader/PdmReader/Models/GetFiles.cs
@@ -30,5 +30,28 @@ namespace PdmReader.Models {
         public static IEnumerable<string> GetPdmFiles(string dir) {
             return GetPdmFiles(dir, "*");
         }
+
+        /// <summary>
+        /// 获取指定文件夹内所有文件
+        /// </summary>
+        /// <param name="dir">文件夹路径</param>
+        /// <param name="searchPattern">模式匹配正则表达式</param>
+        /// <returns>返回文件路径</returns>
+        public static List<string> GetPdmFileAll(string dir, string searchPattern) {
+            if(!Directory.Exists(dir)) {
+                MessageBox.Show("指定文件夹不存在！");
+                return null;
+            }
+            return Directory.GetFileSystemEntries(dir, searchPattern, SearchOption.AllDirectories).Where(File.Exists).ToList();
+        }
+
+        /// <summary>
+        /// 获取指定文件夹内所有文件
+        /// </summary>
+        /// <param name="dir">文件夹路径</param>
+        /// <returns>返回文件路径</returns>
+        public static List<string> GetPdmFileAll(string dir) {
+            return GetPdmFileAll(dir, "*");
+        }
     }
 }
